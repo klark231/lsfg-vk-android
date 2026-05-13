ab0155394c44fbc1d20b5390c792daba2fd7e09e
klark231 <klarkperalta@gmail.com>
2026-05-13 13:40:18 +0800

    Change dstAccessMask to use shader read access

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -171,7 +171,7 @@ void Utils::copyImage(VkCommandBuffer buf,
         const VkImageMemoryBarrier presentBarrier{
             .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
             .srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
-            .dstAccessMask = VK_ACCESS_MEMORY_READ_BIT,
+            .dstAccessMask = VK_ACCESS_SHADER_READ_BIT,
             .oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
             .newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
             .image = dst,
